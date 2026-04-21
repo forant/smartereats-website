c2b61dfe8a967405f0205bc0161b59dcc467e11e
Tim Foran <tforan@gmail.com>
2026-04-20 20:53:37 -0700

    Next.js static export config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // 👈 THIS is the key
+
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true,
+    unoptimized: true, // required for static export
   },
 }
 
